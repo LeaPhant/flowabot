49a81dad26ddf3431c450a2ee88ca93a7aba3b15
Lea <14080165+LazyLea@users.noreply.github.com>
2019-07-03 21:51:15 +0200

    add COMMANDS.md generator

--- generate-commands-md.js
+++ generate-commands-md.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const config = require('./config.json');
 
 let commands = [];
 let commands_path = path.resolve(__dirname, 'commands');
+
+let output = `# Commands
+### Table of contents`;
 
 fs.readdir(commands_path, (err, items) => {
 
@@ -19,7 +23,42 @@ fs.readdir(commands_path, (err, items) => {
             command.command = [command.command];
     });
 
-    commands = commands.sort((a, b) => a.command[0] > b.command[0]);
+    commands.forEach(command => {
+        output += `\n- [${config.prefix}${command.command[0]}](#${command.command[0]})`;
+    });
 
-    console.log(commands);
+    output += `\n---`
+
+    commands.forEach(command => {
+        output += `\n## ${config.prefix}${command.command[0]}`;
+
+        if(command.description)
+            output += `\n${command.description}`;
+
+        if(command.command.length > 1)
+            output += `\n\n**Variations**: \`${config.prefix}${command.command.join('`, ``' + config.prefix)}\``;
+
+        output += `\n\n**Usage**: \`${config.prefix}${command.command[0]}`;
+
+        if(command.usage)
+            output += ` ${command.usage}`;
+
+        output += '`';
+
+        if(command.example){
+            if(!Array.isArray(command.example))
+                command.example = [command.example];
+
+            output += `\n### Example${command.example.length > 1 ? 's' : ''}:`;
+
+            command.example.forEach(example => {
+                output += `\n\n    ${config.prefix}${example.run}`;
+                output += `\n${example.result}`;
+            });
+        }
+
+    });
+
+    fs.writeFileSync('COMMANDS.md', output);
+    process.exit(0);
 });
