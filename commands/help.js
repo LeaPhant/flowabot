4d51e43aeec769cdb814187836e8a3d4d306c6f8
LeaPhant <mail@lea.moe>
2019-07-10 17:54:27 +0200

    add commands list to !help

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,10 @@ const config = require('../config.json');
 module.exports = {
     command: 'help',
     argsRequired: 1,
-    description: "Get help for a command.",
+    description: [
+        "Get help for a command.",
+        "List of all commands: https://github.com/LeaPhant/flowabot/blob/master/COMMANDS.md"
+    ],
     usage: '<command>',
     example: [
         {
