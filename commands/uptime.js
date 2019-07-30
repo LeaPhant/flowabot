01f846e688fe1467675168e20df567ac2793ce71
LeaPhant <mail@lea.moe>
2019-07-30 21:31:53 +0200

    fix !uptime and !downtime

--- commands/uptime.js
+++ commands/uptime.js
@@ -6,8 +6,12 @@ const helper = require('../helper.js');
 const config = require('../config.json');
 
 const twitchKraken = axios.create({
-    baseURL: 'https://api.twitch.tv/kraken'
-})
+    baseURL: 'https://api.twitch.tv/kraken',
+    headers: {
+        'Accept': 'application/vnd.twitchtv.v5+json',
+        'Client-ID': config.credentials.twitch_client_id
+    }
+});
 
 module.exports = {
     command: ['uptime', 'downtime'],
@@ -31,41 +35,60 @@ module.exports = {
 
             let channel_name = argv[1];
 
-            twitchKraken.get(`/channels/${channel_name}/videos`, {
+            twitchKraken.get(`/users`, {
                 params: {
-                    client_id: config.credentials.twitch_client_id,
-                    broadcast_type: 'archive',
-                    limit: 1
-                }
+                    'login': channel_name,
+                },
             }).then(response => {
-                let videos = response.data.videos;
-                if(videos.length >= 1){
-                    var vod = videos[0];
-                    var name = vod.channel.display_name;
-                    if(vod.status == 'recording'){
-                        resolve(
-                            `${name} has been live for ${moment.duration(moment().unix() - moment(vod.created_at).unix(), "seconds").format("h [hour and] m [minute]")}
-                        `);
-                    }else{
-                        let duration = moment().unix() - (moment(vod.created_at).unix() + vod.length);
+                let users = response.data.users;
 
-                        if(duration < 60){
+                if(users.length == 0){
+                    reject('User not found');
+                    return;
+                }
+
+                let user_id = users[0]._id;
+
+                console.log('USER ID', user_id);
+
+                twitchKraken.get(`/channels/${user_id}/videos`, {
+                    params: {
+                        broadcast_type: 'archive',
+                        limit: 1
+                    }
+                }).then(response => {
+                    let videos = response.data.videos;
+                    if(videos.length >= 1){
+                        var vod = videos[0];
+                        var name = vod.channel.display_name;
+                        if(vod.status == 'recording'){
                             resolve(
-                                `${name} hasn't streamed in ${moment.duration(duration, 'seconds').format('s [second]')}
-                            `);
-                        }else if(duration < 60 * 60){
-                            resolve(
-                                `${name} hasn't streamed in ${moment.duration(duration, 'seconds').format('m [minute]')}
+                                `${name} has been live for ${moment.duration(moment().unix() - moment(vod.created_at).unix(), "seconds").format("h [hour and] m [minute]")}
                             `);
                         }else{
-                            resolve(
-                                `${name} hasn't streamed in ${moment.duration(duration, 'seconds').format('d [day and] h [hour]')}
-                            `);
+                            let duration = moment().unix() - (moment(vod.created_at).unix() + vod.length);
+
+                            if(duration < 60){
+                                resolve(
+                                    `${name} hasn't streamed in ${moment.duration(duration, 'seconds').format('s [second]')}
+                                `);
+                            }else if(duration < 60 * 60){
+                                resolve(
+                                    `${name} hasn't streamed in ${moment.duration(duration, 'seconds').format('m [minute]')}
+                                `);
+                            }else{
+                                resolve(
+                                    `${name} hasn't streamed in ${moment.duration(duration, 'seconds').format('d [day and] h [hour]')}
+                                `);
+                            }
                         }
+                    }else{
+                        reject(`User either has VODs disabled or hasn't streamed in a while`);
                     }
-                }else{
-                    reject(`User either has VODs disabled or hasn't streamed in a while`);
-                }
+                }).catch(err => {
+                    reject('User not found');
+                    helper.error(err);
+                });
             }).catch(err => {
                 reject('User not found');
                 helper.error(err);
