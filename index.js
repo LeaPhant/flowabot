bda4eabf0f66bd6369e3b52112a3338b5ad2912f
LeaPhant <mail@lea.moe>
2021-02-04 23:11:16 +0100

    Oops

--- index.js
+++ index.js
@@ -307,10 +307,6 @@ function onMessage(msg){
 
 client.on('message', onMessage);
 
-client.on('presenceUpdate', (oldPresence, newPresence) => {
-    console.log(newPresence);
-});
-
 client.on('ready', () => {
 	helper.log('flowabot is ready');
 	if(config.credentials.discord_client_id)
