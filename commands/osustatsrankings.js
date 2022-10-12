6041ec97933c80300ad42da6b5bb69333a88280b
respektive <liambzw@gmail.com>
2022-10-13 01:27:38 +0200

    fix type check

--- commands/osustatsrankings.js
+++ commands/osustatsrankings.js
@@ -152,7 +152,7 @@ module.exports = {
                 if (user) {
                     const res = await axios.get(`https://osustats.respektive.pw/counts/${user}${searchParamsString}`)
                     user_row = res.data
-                    if (user_row && user_row.username && user_row[type] > (rankings[0]?.type ?? 0)) {
+                    if (user_row && user_row.username && user_row[type] > (rankings[0][type] ?? 0)) {
                         output += `\`#${user_row[`${type}_rank`] ?? "??"}${user_row[`${type}_rank`] < 10 ? " " : ""}\``
                         let country_code = user_row.country?.toLowerCase() ?? null
                         output += country_code ? `:flag_${country_code}:` : ":pirate_flag:"
@@ -169,7 +169,7 @@ module.exports = {
                     output += ` \`${user[type].toLocaleString()}${" ".repeat(clamp(biggest_count - user[type].toString().length, 0, biggest_count))}\`\n`
                 }
 
-                if (user_row && user_row.username && user_row[type] < (rankings[rankings.length - 1]?.type ?? 0)) {
+                if (user_row && user_row.username && user_row[type] < (rankings[rankings.length - 1][type] ?? 0)) {
                     output += `\`#${user_row[`${type}_rank`] ?? "??"}${user_row[`${type}_rank`] < 10 ? " " : ""}\``
                     let country_code = user_row.country?.toLowerCase() ?? null
                     output += country_code ? `:flag_${country_code}:` : ":pirate_flag:"
