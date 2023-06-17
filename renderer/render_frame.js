0efe114bb4ecfcec6648043fc62a26585e037677
respektive <liambzw@gmail.com>
2023-06-17 21:43:16 +0200

    actually fix pekli upload

--- renderer/render_frame.js
+++ renderer/render_frame.js
@@ -848,9 +848,8 @@ module.exports = {
                                         [
                                             '-s', '-X', 'POST', 'https://pek.li/api/upload',
                                             '-H', 'content-type: multipart/form-data',
-                                            '-H', 'accept: application/vnd.chibisafe.json',
                                             '-H', 'x-api-key: ' + config.credentials.pekli_token,
-                                            '-F', `files[]=@${file_path}/video.${options.type}`
+                                            '-F', `file[]=@${file_path}/video.${options.type}`
                                         ]);
 
                                     const json = JSON.parse(response.stdout);
