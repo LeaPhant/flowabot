abf5b7988e95efce510afd96d72823548b64af97
LeaPhant <mail@lea.moe>
2024-12-31 01:50:25 +0100

    Heavily improve slider path accuracy

--- renderer/beatmap/slider.js
+++ renderer/beatmap/slider.js
@@ -1,11 +1,14 @@
 const { 
 	vectorMultiply, vectorDivide, vectorAdd, vectorSubtract, 
-	vectorDistance, vectorDistanceSquared 
+	vectorDistance, vectorDistanceSquared, 
+	vectorEquals
 } = require('./util');
 
 const CATMULL_DETAIL = 50;
 const CIRCULAR_ARC_TOLERANCE = 0.1;
 const BEZIER_DETAIL = 100;
+
+const TAIL_LENIENCY = -36;
 
 const catmullFindPoint = (vec1, vec2, vec3, vec4, t) => {
     let t2 = t * t;
@@ -53,6 +56,168 @@ const coordsOnBezier = (pointArray, t) => {
 	return [bx,by];
 }
 
+// interpolate points across an even distance d
+const interpolatePoints = (points, d = 1) => {
+	// first generate distance data
+	let length = 0;
+
+	points = points.map((point, i) => {
+		if (i == 0)
+			return { point, distance: 0 };
+
+		const distance = vectorDistance(point, points[i - 1]);
+		length += distance;
+
+		return { point, distance };
+	});
+
+	// here our interpolated points will go, each exactly 1 d apart 
+	const dots = [points[0].point];
+
+	let distance = 0;
+	let curr = points[0];
+	let nextIndex = 1;
+	let lastIndex = points.length - 1;
+	let next = points[nextIndex];
+	let currDistance = next.distance;
+
+	while (distance < length) {
+		// go across the curve until a point is at least 1 d away (or we're out of points)
+		while (currDistance < d && nextIndex < lastIndex) {
+			nextIndex++;
+			next = points[nextIndex];
+			currDistance += next.distance;
+		}
+
+		// we've reached the last point and there are no more points to satisfy distance d, abort
+		if (currDistance < d) break;
+
+		// go across distance in intervals of d until we reach the next point
+		while (currDistance >= d) {
+			// nudge the current point 1 pixel into the direction of the next point 
+			const relVector = vectorSubtract(next.point, curr.point);
+			const nudgeVector = vectorMultiply(relVector, d / currDistance);
+			const nudgedPoint = vectorAdd(curr.point, nudgeVector)
+
+			dots.push(nudgedPoint);
+
+			currDistance -= d;
+			distance += d;
+			curr = { point: nudgedPoint, distance: d };
+		}
+	}
+
+	return dots;
+};
+
+// Pretty much copied from osu-lazer https://github.com/ppy/osu-framework/blob/master/osu.Framework/MathUtils/PathApproximator.cs#L89
+const getCatmullCurve = curve => {
+	const points = [];
+
+	for (let x = 0; x < curve.length - 1; x++) {
+		const v1 = x > 0 ? curve[x - 1] : curve[x];
+		const v2 = curve[x];
+		const v3 = x < curve.length - 1 ? curve[x + 1] : vectorSubtract(vectorAdd(v2, v2), v1);
+		const v4 = x < curve.length - 2 ? curve[x + 2] : vectorSubtract(vectorAdd(v3, v3), v2);
+
+		for (let c = 0; c < CATMULL_DETAIL; c++) {
+			points.push(
+				catmullFindPoint(v1, v2, v3, v4, c / CATMULL_DETAIL),
+				catmullFindPoint(v1, v2, v3, v4, (c + 1) / CATMULL_DETAIL)
+			);
+		}
+	}
+
+	return points;
+};
+
+const getBezierCurve = curve => {
+	const points = [];
+
+	// generate points on a bezier curve
+	for (let x = 0; x <= 1; x += 1 / BEZIER_DETAIL)
+		points.push(coordsOnBezier(curve, x));
+
+	return points;
+};
+
+// Pretty much copied from osu-lazer https://github.com/ppy/osu-framework/blob/master/osu.Framework/MathUtils/PathApproximator.cs#L114
+const get3PointCurve = curve => {
+	const points = [];
+	const [a, b, c] = curve;
+
+	const aSq = vectorDistanceSquared(b, c);
+	const bSq = vectorDistanceSquared(a, c);
+	const cSq = vectorDistanceSquared(a, b);
+
+	if (aSq == 0 || bSq == 0 || bSq == 0) {
+		return curve;
+	}
+
+	const s = aSq * (bSq + cSq - aSq);
+	const t = bSq * (aSq + cSq - bSq);
+	const u = cSq * (aSq + bSq - cSq);
+
+	const sum = s + t + u;
+
+	if (sum == 0) {
+		return curve;
+	}
+
+	let center = [
+		s * a[0] + t * b[0] + u * c[0],
+		s * a[1] + t * b[1] + u * c[1]
+	];
+
+	center = vectorDivide(center, sum);
+
+	const dA = vectorSubtract(a, center);
+	const dC = vectorSubtract(c, center);
+
+	const r = vectorDistance(a, center);
+
+	let thetaStart = Math.atan2(dA[1], dA[0]);
+	let thetaEnd = Math.atan2(dC[1], dC[0]);
+
+	while (thetaEnd < thetaStart)
+		thetaEnd += 2 * Math.PI;
+
+	let dir = 1;
+	let thetaRange = thetaEnd - thetaStart;
+
+	let orthoAtoC = vectorSubtract(c, a);
+
+	orthoAtoC = [
+		orthoAtoC[1],
+		-orthoAtoC[0]
+	];
+
+	let bMinusA = vectorSubtract(b, a);
+
+	if (orthoAtoC[0] * bMinusA[0] + orthoAtoC[1] * bMinusA[1] < 0) {
+		dir = -dir;
+		thetaRange = 2 * Math.PI - thetaRange;
+	}
+
+	const amountPoints = Math.max(25, 2 * r <= CIRCULAR_ARC_TOLERANCE ? 2 : Math.max(2, Math.ceil(thetaRange / (2 * Math.acos(1 - CIRCULAR_ARC_TOLERANCE / r)))));
+
+	for (let i = 0; i < amountPoints; ++i) {
+		const fract = i / (amountPoints - 1);
+		const theta = thetaStart + dir * fract * thetaRange;
+
+		let o = [
+			Math.cos(theta),
+			Math.sin(theta)
+		];
+
+		o = vectorMultiply(o, r);
+
+		points.push(vectorAdd(center, o))
+	}
+
+	return points;
+}
+
 class SliderProcessor {
 	Beatmap;
 	Slider;
@@ -61,190 +226,92 @@ class SliderProcessor {
 		this.Beatmap = Beatmap;
 	}
 
-	// Pretty much copied from osu-lazer https://github.com/ppy/osu-framework/blob/master/osu.Framework/MathUtils/PathApproximator.cs#L114
 	apply3Point () {
 		const { Slider } = this;
-		const [a, b, c] = this.Slider.points;
 
-		let aSq = vectorDistanceSquared(b, c);
-		let bSq = vectorDistanceSquared(a, c);
-		let cSq = vectorDistanceSquared(a, b);
-
-		if (aSq == 0 || bSq == 0 || bSq == 0) {
-			Slider.SliderDots = [...Slider.points];
-			return;
-		}
-
-		let s = aSq * (bSq + cSq - aSq);
-		let t = bSq * (aSq + cSq - bSq);
-		let u = cSq * (aSq + bSq - cSq);
-
-		let sum = s + t + u;
-
-		if (sum == 0) {
-			Slider.SliderDots = [...Slider.points];
-			return;
-		}
-
-		let center = [
-			s * a[0] + t * b[0] + u * c[0],
-			s * a[1] + t * b[1] + u * c[1]
-		];
-
-		center = vectorDivide(center, sum);
-
-		let dA = vectorSubtract(a, center);
-		let dC = vectorSubtract(c, center);
-
-		let r = vectorDistance(a, center);
-
-		let thetaStart = Math.atan2(dA[1], dA[0]);
-		let thetaEnd = Math.atan2(dC[1], dC[0]);
-
-		while (thetaEnd < thetaStart)
-			thetaEnd += 2 * Math.PI;
-
-		let dir = 1;
-		let thetaRange = thetaEnd - thetaStart;
-
-		let orthoAtoC = vectorSubtract(c, a);
-
-		orthoAtoC = [
-			orthoAtoC[1],
-			-orthoAtoC[0]
-		];
-
-		let bMinusA = vectorSubtract(b, a);
-
-		if (orthoAtoC[0] * bMinusA[0] + orthoAtoC[1] * bMinusA[1] < 0) {
-			dir = -dir;
-			thetaRange = 2 * Math.PI - thetaRange;
-		}
-
-		let amountPoints = Math.max(25, 2 * r <= CIRCULAR_ARC_TOLERANCE ? 2 : Math.max(2, Math.ceil(thetaRange / (2 * Math.acos(1 - CIRCULAR_ARC_TOLERANCE / r)))));
-
-		for (let i = 0; i < amountPoints; ++i) {
-			let fract = i / (amountPoints - 1);
-			let theta = thetaStart + dir * fract * thetaRange;
-
-			let o = [
-				Math.cos(theta),
-				Math.sin(theta)
-			];
-
-			o = vectorMultiply(o, r);
-
-			Slider.SliderDots.push(vectorAdd(center, o));
-		}
+		const points = get3PointCurve(Slider.points);
+		Slider.SliderDots.push(...interpolatePoints(points));
 	}
 
-	// Pretty much copied from osu-lazer https://github.com/ppy/osu-framework/blob/master/osu.Framework/MathUtils/PathApproximator.cs#L89
 	applyCatmull () {
 		const { Slider } = this;
 
-		for (let x = 0; x < Slider.points.length - 1; x++) {
-			const v1 = x > 0 ? Slider.points[x - 1] : Slider.points[x];
-			const v2 = Slider.points[x];
-			const v3 = x < Slider.points.length - 1 ? Slider.points[x + 1] : vectorSubtract(vectorAdd(v2, v2), v1);
-			const v4 = x < Slider.points.length - 2 ? Slider.points[x + 2] : vectorSubtract(vectorAdd(v3, v3), v2);
-
-			for (let c = 0; c < CATMULL_DETAIL; c++) {
-				Slider.SliderDots.push(
-					catmullFindPoint(v1, v2, v3, v4, c / CATMULL_DETAIL),
-					catmullFindPoint(v1, v2, v3, v4, (c + 1) / CATMULL_DETAIL)
-				);
-			}
-		}
+		const catmullPoints = getCatmullCurve(Slider.points);
+		Slider.SliderDots.push(...interpolatePoints(catmullPoints));
 	}
 
 	applyBezier () {
 		const { Slider } = this;
+		const { points } = Slider;
 
-		let sliderParts = [];
+		// iterate through slider points and split them into separate parts
+		// at duplicate red points
+		const sliderParts = [];
         let sliderPart = [];
 
-		for (const [index, point] of Slider.points.entries()) {
+		for (const [i, point] of points.entries()) {
 			sliderPart.push(point);
-			if(index < Slider.points.length - 1){
-				if(point[0] == Slider.points[index + 1][0] && point[1] == Slider.points[index + 1][1]){
+
+			if(i < points.length - 1){
+				if (vectorEquals(point, points[i + 1])) {
 					sliderParts.push(sliderPart);
 					sliderPart = [];
 				}
-			}else if(Slider.points.length - 1 == index){
-				sliderPart.push(point);
-				sliderParts.push(sliderPart);
-			}
-		}
 
-		for (const part of sliderParts) {
-			if (part.length == 2) {
-				Slider.SliderDots.push(part[0], part[1]);
 				continue;
 			}
-			
-			for (let x = 0; x <= 1; x += 1 / BEZIER_DETAIL)
-				Slider.SliderDots.push(coordsOnBezier(part, x));
+
+			sliderPart.push(point);
+			sliderParts.push(sliderPart);
 		}
+
+		const dots = [];
+
+		// iterate through slider parts and generate curves with equal distance
+		for (const part of sliderParts) {
+			// two red control points in a row, this is a line
+			if (part.length == 2) {
+				const linearPoints = [part[0], part[1]];
+
+				dots.push(...interpolatePoints(linearPoints));
+				continue;
+			}
+
+			const bezierPoints = getBezierCurve(part);
+			dots.push(...interpolatePoints(bezierPoints));
+		}
+
+		Slider.SliderDots.push(...dots);
 	}
 
-	interpolateDots () {
+	applyActualEnd () {
 		const { Slider } = this;
+		const { SliderDots } = Slider;
 
-		const SliderDots = [];
+		const turnDuration = Slider.duration / Slider.repeatCount;
 
-		let pos_current = Slider.SliderDots[0];
-        let next_index = 1;
-        let pos_next = Slider.SliderDots[next_index];
-        let length = 0;
+		const finalSpanIndex = Slider.repeatCount - 1;
+        const finalSpanStartTime = Slider.startTime + finalSpanIndex * turnDuration;
 
-        while (next_index < Slider.SliderDots.length - 1 && length < Slider.pixelLength) {
-            while (vectorDistanceSquared(pos_current, pos_next) < 1 * 1 && next_index < Slider.SliderDots.length - 1) {
-                next_index++;
-                pos_next = Slider.SliderDots[next_index];
-            }
+		const legacyLastTickTime = Math.max(Slider.startTime + Slider.duration / 2, (finalSpanStartTime + turnDuration) + TAIL_LENIENCY);
+        let legacyLastTickProgress = (legacyLastTickTime - finalSpanStartTime) / turnDuration;
 
-            const distance = vectorDistance(pos_current, pos_next);
+        if (Slider.repeatCount % 2 == 0)
+			legacyLastTickProgress = 1 - legacyLastTickProgress;
 
-			if (distance < 1) continue;
+		Slider.actualEndTime = legacyLastTickTime;
+		Slider.actualEndPosition = SliderDots[Math.floor(legacyLastTickProgress * (SliderDots.length - 1))];
 
-			const pos_interpolated = [
-				pos_current[0] + (1 / distance) * (pos_next[0] - pos_current[0]),
-				pos_current[1] + (1 / distance) * (pos_next[1] - pos_current[1])
-			];
-
-			SliderDots.push(pos_interpolated);
-
-			pos_current = pos_interpolated;
-			length++;
-        }
-
-        const turnDuration = Slider.duration / Slider.repeatCount;
-
-        if(turnDuration < 72){
-            Slider.actualEndPosition = SliderDots[Math.floor(SliderDots.length / 2 - 1)];
-            Slider.actualEndTime = Slider.startTime + (Slider.repeatCount - 1) * turnDuration + turnDuration / 2;
-        }else{
-            const sliderDotDuration = turnDuration / SliderDots.length;
-
-            const turnSliderDots = Slider.repeatCount % 2 == 0 ? SliderDots.slice().reverse() : SliderDots;
-
-            Slider.actualEndTime = Slider.endTime - 36;
-            Slider.actualEndPosition = turnSliderDots[Math.floor(turnSliderDots.length - 1 - 36 / sliderDotDuration)];
-        }
-
-		if (SliderDots.length < 2) {
+		/*if (SliderDots.length < 2) {
 			Slider.SliderDots = [...Slider.points];
-            return;
-		}
-
-        Slider.SliderDots = SliderDots;
+		}*/
 	}
 
 	generateTicks () {
 		const { Beatmap, Slider } = this;
 
 		Slider.endPosition = Slider.SliderDots[Slider.SliderDots.length - 1];
-		
+
 		// How far away you can stay away from the slider end without missing it
 		let lazyEndOffset = Math.floor(Beatmap.ActualFollowpointRadius);
 
@@ -300,7 +367,7 @@ class SliderProcessor {
 			this.applyBezier();
 		}
 
-		this.interpolateDots();
+		this.applyActualEnd();
 		this.generateTicks();
 	}
 }
